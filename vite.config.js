e6ba9a005b4767ca94c09a5465e901f07aa106a3
Sumin Jin <him7509@gmail.com>
2026-05-04 12:45:32 +0900

    Configure Electron desktop builds

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/twrpg-helper/",
+  base: process.env.ELECTRON_BUILD ? "./" : "/twrpg-helper/",
   plugins: [react()],
 });
